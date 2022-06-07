548253deb2ad43be04b103ed9cfb31c8b5da1e2b
Jokeseph <josephofilii@gmail.com>
2022-06-07 13:59:37 +0100

    state componenets

--- src/components/SearchbarUi.js
+++ src/components/SearchbarUi.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react'
 import '../styles/SearchbarUi.css'
 
 export class SearchbarUi extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         search:'',
+      }
+    }
+
+    handleSearchChange = (e) => {
+        this.setState({
+            search: e.target.value,
+        })
+    }
+
   render() {
+      const { search } = this.state;
     return (
         <div className='body'>
             <div className="search-box">
                 <button className="btn-search">🔎</button>
-                <input type="text" className="input-search" placeholder="Type to Search..." />
+                <input value={search} onChange = {(e) => this.handleSearchChange(e)} type="text" className="input-search" placeholder="Type to Search..." />
             </div>
         </div>
     )
